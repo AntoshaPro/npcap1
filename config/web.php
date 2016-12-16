ad75e71968780926fdd9d77f4a1041960c08f0a3
AntoshaPro <proskurin@npc.local>
2016-12-16 14:14:39 +0300

    В этой заметке я опишу повстречавшиеся мне подводные камни при настройке ЧПУ для Yii2.
    После пары дней пользования фреймворком решил наконец настроить маршрутизацию "по-человечески", чтобы ссылки выглядели не http://domain/?r=users/create, а http://domain/users/create. На такой случай под рукой всегда есть guide, в частности быстро находится нужная информация (Using pretty URLs) про "очеловечивание" урлов. Не долго думая, наваял в конфиг по примеру из гайда:
    
    'urlManager' => [
                'enablePrettyUrl' => true,
                'showScriptName' => false,
                'enableStrictParsing' => true,
                'rules' => [
                    'users' => 'users/index',
                    'users/' => 'users/view'
                ],
    
     Но ничего, как можно догадаться, не сработало. Почти сразу обратил внимание на отсутствие .htaccess файла в директории /web (использовал basic шаблон). Создадим .htaccess:
    
    RewriteEngine on
    
    # If a directory or a file exists, use it directly
    RewriteCond %{REQUEST_FILENAME} !-f
    RewriteCond %{REQUEST_FILENAME} !-d
    # Otherwise forward it to index.php
    RewriteRule . index.php
    
    Еще попытка обратиться по адресу  http://domain/users или  http://domain/users/user_id - провал.
    Пришла мысль что, возможно, поддержка htaccess файлов отключена (не просто так, между прочим) и нужно лезть в конфиги apache2 (привык за долгое время к виндовым denwer и openserver, там, обычно, многое уже включено и настроено). Итак, открываем конфиг хоста domain (/etc/apache2/sites-available/domain.conf) и добавляем в него волшебные строки:
    
    <Directory /var/www/domain/web/>
     Options Indexes FollowSymLinks
     AllowOverride All
     Require all granted
    </Directory>
    
    где "/var/www/domain/web/" - путь до каталога, в котором лежит входной скрипт yii2. AllowOverride All говорит серверу, использовать htaccess файлы в указанной директории.
    
    Осталось только перезагрузить apache2:
    
    service apache2 reload
    
    Пробуем - опять ничего! Конечно! Забыл включить модуль rewrite:
    
    a2enmod rewrite
    service apache2 reload
    Вот теперь всё работает. Так что стоит привыкать, что что-то может быть по дефолту отрублено.
    
    P.S. Эта заметка описывает решение конкретной проблемы, для более полной информации о ЧПУ Yii2 изучайте документацию. Еще вот повстречал статейку на русском: http://coderlife.eu/php/yii2-urlmanager-setup.html
    
    взято отсюда -- http://koi-notes.blogspot.ru/2015/02/yii2-ubuntu-apache-24.html

--- config/web.php
+++ config/web.php
@@ -12,6 +12,8 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'd4QsAEvKGzSG5vBhzSKPlB_-52sO0qKB',
+            'baseUrl'=> '',
+
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -40,14 +42,16 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => false,
             'rules' => [
+                //..
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
