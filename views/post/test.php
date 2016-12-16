9f1bb4ca185ae42d0bcc1097b774d1a74db8bb79
AntoshaPro <proskurin@npc.local>
2016-12-16 11:50:03 +0300

    composer

--- views/post/test.php
+++ views/post/test.php
@@ -18,6 +18,7 @@ use yii\helpers\Html;
 <?php $form = ActiveForm::begin(['options' => ['id'=> 'testForm']]) ?>
 <?= $form->field($model, 'name')->label('Имя')?>
 <?= $form->field($model, 'email')->input('email')?>
+<?= yii\jui\DatePicker::widget(['name' => 'attributeName']) ?>
 <?= $form->field($model, 'text')->label('Текст сообщения')->textarea(['rows'=> 5])?>
 <?= Html::submitButton('Отправить', ['class' => 'btn btn-success'])?>
 <?php ActiveForm::end() ?>
